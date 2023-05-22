58d2bd96c120c3bf7d0f7f7af84c56fc8084bb32
vlknygt <volkanyigittr@gmail.com>
2023-05-22 21:36:40 +0300

    test map

---
+++ yeni_test/test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import optparse
+
+# we need to import some python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+from sumolib import checkBinary  # Checks for the binary in environ vars
+import traci
+import os
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = opt_parser.parse_args()
+    return options
+
+# contains TraCI control loop
+def run():
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+
+
+
+    traci.close()
+    sys.stdout.flush()
+
+# main entry point
+if __name__ == "__main__":
+    options = get_options()
+
+    # check binary
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # traci starts sumo as a subprocess and then this script connects and runs
+    traci.start([sumoBinary, "-c", "test.sumocfg"])
+    run()
