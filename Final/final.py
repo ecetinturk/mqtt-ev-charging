588607e4707d62e2ab6ecc39136a5d3da836dc9b
vlknygt <volkanyigittr@gmail.com>
2023-05-24 23:27:54 +0300

    mqtt functions updated

--- Final/final.py
+++ Final/final.py
@@ -3,6 +3,13 @@
 import os
 import sys
 import optparse
+from sumolib import checkBinary  # Checks for the binary in environ vars
+import random
+import traci
+import time
+import os
+import paho.mqtt.client as mqtt
+import threading
 
 # we need to import some python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -11,20 +18,107 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
+broker="127.0.0.1"
+port =1884
+ssl_port=8883 #ssl
 
-from sumolib import checkBinary  # Checks for the binary in environ vars
-import random
-import time
-import traci
-import os
-import paho.mqtt.client as mqtt
-from functools import partial
+clients = []
 
-mqttBroker = "127.0.0.1"
-client_ev = mqtt.Client("Electric Vehicle")
-client_cs = mqtt.Client("Charging Station")
-client_cs.connect(mqttBroker, 1884)
-client_ev.connect(mqttBroker, 1884)
+Normal_connections=5
+SSL_Connections=0 #no ssl connections illustration only
+message="test message"
+topic="BATTERY"
+out_queue=[] #use simple array to get printed messages in some form of order
+def on_log(client, userdata, level, buf):
+   print(buf)
+def on_message(client, userdata, message):
+   time.sleep(1)
+   msg="message received",str(message.payload.decode("utf-8"))
+   #print(msg)
+   out_queue.append(msg)
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        client.subscribe(topic)
+    else:
+        print("Bad connection Returned code=",rc)
+        client.loop_stop()
+def on_disconnect(client, userdata, rc):
+   pass
+   #print("client disconnected ok")
+def on_publish(client, userdata, mid):
+   time.sleep(1)
+   print("In on_pub callback mid= "  ,mid)
+
+
+def Create_connections(nclients,port,SSL_Flag=False):
+   for i in range(nclients):
+      if SSL_Flag:
+         cname ="python-ssl"+str(i)
+      else:
+         cname ="python-"+str(i)
+      client = mqtt.Client(cname)             #create new instance
+
+      if SSL_Flag:
+         client.tls_set('c:/python34/steve/MQTT-demos/certs/ca-pi.crt')
+      #client.connected_flag=False #create flag in client
+
+      client.connect(broker,port)           #establish connection
+      #client.on_log=on_log #this gives getailed logging
+      client.on_connect = on_connect
+      client.on_disconnect = on_disconnect
+      #client.on_publish = on_publish
+      client.on_message = on_message
+      clients.append(client)
+      client.loop_start()
+      while not client.connected_flag:
+         time.sleep(0.05)
+
+def send_message():
+    mqtt.Client.connected_flag = False  # create flag in class
+    no_threads = threading.active_count()
+    print("current threads =", no_threads)
+    print("Creating Normal Connections ", Normal_connections, " clients")
+    Create_connections(Normal_connections, port, False)
+    if SSL_Connections != 0:
+        print("Creating SSL Connections ", SSL_Connections, " clients")
+        Create_connections(SSL_Connections, ssl_port, True)
+
+    print("All clients connected ")
+    time.sleep(5)
+    #
+    count = 0
+    no_threads = threading.active_count()
+    print("current threads =", no_threads)
+    print("Publishing ")
+
+    Run_Flag = True
+    try:
+        while Run_Flag:
+            i = 0
+            for client in clients:
+                counter = str(count).rjust(6, "0")
+                msg = "client " + str(i) + " " + counter + "XXXXXX " + message
+
+                client.publish(topic, msg)
+                time.sleep(0.1)
+                print("publishing client " + str(i))
+                i += 1
+            time.sleep(10)  # now print messages
+            print("queue length=", len(out_queue))
+            for x in range(len(out_queue)):
+                print(out_queue.pop())
+            count += 1
+            # time.sleep(5)#wait
+    except KeyboardInterrupt:
+        print("interrupted  by keyboard")
+
+    # client.loop_stop() #stop loop
+    for client in clients:
+        client.disconnect()
+        client.loop_stop()
+    # allow time for allthreads to stop before existing
+    time.sleep(10)
 
 def get_options():
     opt_parser = optparse.OptionParser()
@@ -33,47 +127,20 @@ def get_options():
     options, args = opt_parser.parse_args()
     return options
 
-# contains TraCI control loop
 def run():
-    #client2.on_message = on_message
-    while traci.simulation.getMinExpectedNumber() > 0:
-        traci.simulationStep()
 
+    battery_capacity = "1000"
+    traci.vehicle.setParameter("v_0", "device.battery.maximumBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_1", "device.battery.maximumBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_2", "device.battery.maximumBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_3", "device.battery.maximumBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_4", "device.battery.maximumBatteryCapacity", battery_capacity)
 
-    traci.close()
-    sys.stdout.flush()
-
-#traci.vehicle.setChargingStationStop(car_id, cs_id, charging_time)
-def charge(car_id):
-    client_cs.loop_start()
-    client_cs.subscribe("BATTERY")
-    client_cs.on_message = partial(on_message, car_id)
-    #time.sleep(5)
-    client_cs.loop_end()
-
-    current_charge = traci.vehicle.getParameter(car_id, "device.battery.actualBatteryCapacity")
-    client_ev.publish("BATTERY", current_charge)
-
-def on_message(client, userdata, message, car_id):
-    #arac sarj oluyor
-    charge = str(int(str(message.payload.decode("utf-8"))) + 1)
-    traci.vehicle.setParameter(car_id, "device.battery.actualBatteryCapacity", charge)
-    print("Received message: " + str(message.payload.decode("utf-8")) + " Battery Level of EV")
-
-
-def run_test():
-
-    traci.vehicle.setParameter("v_0", "device.battery.maximumBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_1", "device.battery.maximumBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_2", "device.battery.maximumBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_3", "device.battery.maximumBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_4", "device.battery.maximumBatteryCapacity", "1000")
-
-    traci.vehicle.setParameter("v_0", "device.battery.actualBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_1", "device.battery.actualBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_2", "device.battery.actualBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_3", "device.battery.actualBatteryCapacity", "1000")
-    traci.vehicle.setParameter("v_4", "device.battery.actualBatteryCapacity", "1000")
+    traci.vehicle.setParameter("v_0", "device.battery.actualBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_1", "device.battery.actualBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_2", "device.battery.actualBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_3", "device.battery.actualBatteryCapacity", battery_capacity)
+    traci.vehicle.setParameter("v_4", "device.battery.actualBatteryCapacity", battery_capacity)
 
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
@@ -91,11 +158,6 @@ def run_test():
                     route_id = random.choice(traci.route.getIDList())
                 traci.vehicle.setRouteID(car_id, route_id)
 
-            #if current_lane == "" and current_charge < 250:
-                #traci.vehicle.setRouteID(car_id, "")
-            #if current_lane == "" and current_route == "":
-                #traci.vehicle.setRouteID(car_id, "")
-
             current_charge = float(traci.vehicle.getParameter(car_id, "device.battery.actualBatteryCapacity"))
             current_route = traci.vehicle.getRouteID(car_id)
             # station: cs_0
@@ -104,13 +166,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_0", 1000 - current_charge)
             if current_lane == "-368620842#100.25" and current_route == "r_1":
                 traci.vehicle.setRouteID(car_id, "r_8")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_0")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
-
 
             # station: cs_4
             if current_lane == "368620842#10" and current_charge < 350:
@@ -118,12 +173,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_4", 1000 - current_charge)
             if current_lane == "368620842#10.79" and current_route == "r_0":
                 traci.vehicle.setRouteID(car_id, "r_7")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_4")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_5
             if current_lane == "668817633" and current_charge < 350:
@@ -131,12 +180,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_5", 1000 - current_charge)
             if current_lane == "6688176330" and current_route == "r_2":
                 traci.vehicle.setRouteID(car_id, "r_6")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_5")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_7
             if current_lane == "-668817633" and current_charge < 350:
@@ -144,12 +187,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_7", 1000 - current_charge)
             if current_lane == "-6688176330" and current_route == "r_3":
                 traci.vehicle.setRouteID(car_id, "r_7")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_7")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_6
             if current_lane == "369625682#1" and current_charge < 350:
@@ -157,12 +194,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_6", 1000 - current_charge)
             if current_lane == "369625682#1.62" and current_route == "r_4":
                 traci.vehicle.setRouteID(car_id, "r_8")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_6")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_3
             if current_lane == "369628890#10" and current_charge < 350:
@@ -170,12 +201,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_3", 1000 - current_charge)
             if current_lane == "369628890#10.115" and current_route == "r_12":
                 traci.vehicle.setRouteID(car_id, "r_5")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_3")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_2
             if current_lane == "-212712097#5" and current_charge < 350:
@@ -183,12 +208,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_2", 1000 - current_charge)
             if current_lane == "-212712097#40" and current_route == "r_14":
                 traci.vehicle.setRouteID(car_id, "r_6")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_2")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             # station: cs_1
             if current_lane == "212712097#4" and current_charge < 350:
@@ -196,12 +215,6 @@ def run_test():
                 traci.vehicle.setChargingStationStop(car_id, "cs_1", 1000 - current_charge)
             if current_lane == "212712097#5" and current_route == "r_13":
                 traci.vehicle.setRouteID(car_id, "r_9")
-            try:
-                if traci.chargingstation.getVehicleIDs("cs_1")[0] == car_id:
-                    for x in range(1000 - int(current_charge)):
-                        charge(car_id)
-            except:
-                print("")
 
             #sarj biterse
             if traci.vehicle.getParameter(car_id, "device.battery.actualBatteryCapacity") == "0.00":
@@ -228,5 +241,10 @@ if __name__ == "__main__":
     # traci starts sumo as a subprocess and then this script connects and runs
     traci.start([sumoBinary, "-c", "final.sumocfg"])
     #run()
-    run_test()
+    t1 = threading.Thread(target=run)
+    t2 = threading.Thread(target=send_message)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
 
