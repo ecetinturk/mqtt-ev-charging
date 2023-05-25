d17179eea70ba382801eeeb76ed44d38a3c8d1ee
burakcaferler <104163715+burakcaferler@users.noreply.github.com>
2023-05-25 20:06:50 +0300

    Charging Interface Updated

--- Arayuz/env/Scripts/activate.bat
+++ Arayuz/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\volka\Desktop\Github\mqtt-ev-charging\yeni_test\env
+set VIRTUAL_ENV=C:\Users\Engineering Mode\Desktop\BDO_proje\mqtt-ev-charging\Arayuz\env
 
 if not defined PROMPT set PROMPT=$P$G
 
